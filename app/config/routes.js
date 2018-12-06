4e4b4fdc1ac7d2e53d8b5debd66fa18eb5c2c201
Jimoh Damilola <jkdamilola@gmail.com>
2018-12-06 18:34:08 +0100

    Deployment pipeline

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,12 +3,6 @@
 const express = require('express');
 const router = express.Router();
 
-const authController = require('../controllers/auth/auth-controller');
-
 router.get('/', (req, res) => res.json({ title: 'Application API' }));
-
-
-// Auth API
-router.post('/auth/login', authController.login);
 
 module.exports = router;
