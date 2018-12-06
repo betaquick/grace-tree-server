aa17158e7665463fbc10cf42c39417ff8ab3c399
Jimoh Damilola <jkdamilola@gmail.com>
2018-12-06 20:09:05 +0100

    Add appspec.yml

--- bin/install-pkgs.sh
+++ bin/install-pkgs.sh
@@ -2,5 +2,5 @@
 
 export $(cat /home/ec2-user/.env)
 (cd /home/ec2-user/application-server && exec /home/ec2-user/.nvm/versions/node/v8.12.0/bin/npm install)
-(cd /home/ec2-user/application-server && exec /home/ec2-user/.nvm/versions/node/v8.12.0/bin/knex migrate:latest --knexfile ./db/knexfile.js)
-(cd /home/ec2-user/application-server && exec /home/ec2-user/.nvm/versions/node/v8.12.0/bin/knex seed:run --knexfile ./db/knexfile.js)
+#(cd /home/ec2-user/application-server && exec /home/ec2-user/.nvm/versions/node/v8.12.0/bin/knex migrate:latest --knexfile ./db/knexfile.js)
+#(cd /home/ec2-user/application-server && exec /home/ec2-user/.nvm/versions/node/v8.12.0/bin/knex seed:run --knexfile ./db/knexfile.js)
