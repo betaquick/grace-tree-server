f5edcb788eafd213b4508831e5e49f6bdea67c65
Jimoh Damilola <jkdamilola@gmail.com>
2018-12-12 18:44:47 +0100

    Fixed the PR comments

---
+++ constants/table.constants.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const USER_TABLE = 'user';
+const USER_PHONE_TABLE = 'user_phone';
+const USER_EMAIL_TABLE = 'user_email';
+const USER_PROFILE_TABLE = 'user_profile';
+const USER_ADDRESS_TABLE = 'user_address';
+const SCHEDULED_DELIVERY_TABLE = 'scheduled_delivery';
+
+module.exports = {
+  USER_TABLE,
+  USER_PHONE_TABLE,
+  USER_EMAIL_TABLE,
+  USER_PROFILE_TABLE,
+  USER_ADDRESS_TABLE,
+  SCHEDULED_DELIVERY_TABLE
+};
