eea546049a5ac23abe1706019fcf42a071229028
Jimoh Damilola <jkdamilola@gmail.com>
2019-02-26 22:19:24 +0100

    Implement wrong address validator

--- app/services/location/location-service.js
+++ app/services/location/location-service.js
@@ -4,16 +4,21 @@ const googleMapsClient = require('@google/maps').createClient({
   key: process.env.GOOGLE_MAP_KEY,
   Promise: Promise
 });
-
 const error = require('debug')('grace-tree:location-service:error');
 const { throwError } = require('../../controllers/util/controller-util');
 
 const getCoordinates = async address => {
   try {
-    const googleMapResponse = await googleMapsClient.geocode({ address }).asPromise();
+    const googleMapResponse = await googleMapsClient.geocode({ address, components: { country: 'US' } }).asPromise();
     const googleMapResults = googleMapResponse.json.results;
+    const result = googleMapResults[0];
 
-    return googleMapResults[0].geometry.location;
+    if (result.partial_match) {
+      debug(result);
+      throwError(422, 'Error retrieving coordinates from address');
+    }
+
+    return result.geometry.location;
   } catch (err) {
     error(err);
     throwError(422, 'Error retrieving coordinates from address');
