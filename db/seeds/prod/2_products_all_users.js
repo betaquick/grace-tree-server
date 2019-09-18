1152af3228a3d4ff8ed68b0b83b206e7ac4f740c
idiakose O. Sunday <idiakosesunday@gmail.com>
2019-09-18 18:51:24 +0100

    Reseed missing products per user

---
+++ db/seeds/prod/2_products_all_users.js
@@ -0,0 +1,60 @@
+'use strict';
+const _ = require('lodash');
+
+const { USER_PRODUCT_TABLE } = require('../../../constants/table.constants');
+/**
+ * @param {*} knex
+ * @returns {Promise<any>} users in db with current products
+ */
+function findUsersWithIncompleteProducts(knex) {
+  const query = knex
+    .select('userId', 'productId')
+    .from(knex
+      .raw('user_product where userId in (select userId from user_product group by userId having count(distinct(productId)) <>  (select count(*) from product))'));
+  return query;
+}
+
+function getProductIds(knex) {
+  return knex.select('productId')
+    .from('product');
+}
+
+exports.seed = async(knex, Promise) => {
+  try {
+    const [productIds, usersWithProductIds] = await Promise.all([
+      getProductIds(knex),
+      findUsersWithIncompleteProducts(knex)
+    ]);
+
+    const grouped_users_products = (_.chain(usersWithProductIds).groupBy(data => data.userId)).value();
+    const updates = [];
+    for (let userId in grouped_users_products) {
+      if (Object.prototype.hasOwnProperty.call(grouped_users_products, userId)) {
+        userId = parseInt(userId, 10);
+        const missingProductIds = _.difference(
+          productIds.map(({productId}) => productId),
+          (grouped_users_products[userId] || []).map(({productId}) => productId)
+        );
+
+        if (missingProductIds && missingProductIds.length) {
+          console.log(`Adding ${missingProductIds.length} products for user ${userId}`);
+        }
+
+        (missingProductIds || []).forEach(productId => updates.push({ productId, userId, status: 0 }));
+      }
+    }
+
+    if (!updates.length) {
+      console.log('No missing products for any user');
+      return Promise.resolve();
+    }
+
+    return knex(USER_PRODUCT_TABLE)
+      .insert(updates);
+  } catch (error) {
+    console.error('Failed with error: ', error);
+    throw error;
+  }
+};
+
+// select userId, productId from user_product where userId in (select userId from user_product group by userId having count(distinct(productId)) <>  (select count(*) from product))
