df49e231dbadf4e8c7ef7359c06a6f7af21d6d82
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-01-20 20:58:45 +0100

    Send terms & conditions to user

--- app/services/messaging/email-service.js
+++ app/services/messaging/email-service.js
@@ -267,6 +267,56 @@ This is to notify you that the delivery scheduled by ${options.companyName} will
   return sendMail(mailOptions);
 };
 
+const sendNotificationOfToCAcceptance = (options) => {
+  const mailOptions = {
+    from: process.env.ADMIN_EMAIL,
+    to: options.email,
+    subject: 'ChipDump Terms and Conditions',
+    text: `
+
+    I understand that by signing up to participate in ChipDump, a totally free program, I am agreeing to the following:
+
+    I am responsible to maintain my profile up to date with all of my current information.
+
+    I am responsible to maintain my correct status for a delivery - “Ready” or “Pause”
+
+    I am responsible to mark all attempted deliveries as successful or unsuccessful on my account
+
+    *ChipDump Arborist Products & Services (AKA gracetreeservices.com), is a free,
+    information management service designed to put people that want Arborist & other products in touch with people and/or 
+    companies (Mostly Arborists & Landscapers), that have those products to give away.
+    ChipDump has no employees and has no authority over the Arborists/Landscapers and others that participate in the program.
+    They subscribe to the program just like you do. Should any issues or problems arise or result regarding any aspect of a delivery, 
+    those should be resolved between the recipient and the delivering party/Arborist/Landscaper or whoever (company or driver), 
+    that is making the delivery. 
+
+    I have read, understand and agree with all of the Terms and Conditions of the ChipDump program, signified by my checking of each one.
+
+    `,
+
+    html: `
+    I understand that by signing up to participate in ChipDump, a totally free program, I am agreeing to the following:
+
+    <ul>
+
+    <li>I am responsible to maintain my profile up to date with all of my current information.</li>
+
+    <li>I am responsible to maintain my correct status for a delivery - “Ready” or “Pause”</li>
+
+    <li>I am responsible to mark all attempted deliveries as successful or unsuccessful on my account</li>
+
+    <li>*ChipDump Arborist Products & Services (AKA gracetreeservices.com), is a free, information management service designed to put people that want Arborist & other products in touch with people and/or companies (Mostly Arborists & Landscapers), that have those products to give away. ChipDump has no employees and has no authority over the Arborists/Landscapers and others that participate in the program. They subscribe to the program just like you do. Should any issues or problems arise or result regarding any aspect of a delivery, those should be resolved between the recipient and the delivering party/Arborist/Landscaper or whoever (company or driver), that is making the delivery.</li> 
+
+    <li>I have read, understand and agree with all of the Terms and Conditions of the ChipDump program, signified by my checking of each one.</li>
+
+    </ul>
+
+    `
+  };
+
+  return sendMail(mailOptions);
+};
+
 module.exports = {
   transporter,
   sendResetMail,
@@ -281,5 +331,6 @@ module.exports = {
   sendAdminNotificationOfRegistration,
   sendAdminNotificationOfRegistrationInExcelFormat,
   sendAdminNotificationOfEstimateOptIn,
+  sendNotificationOfToCAcceptance,
   sendGenericMail
 };
