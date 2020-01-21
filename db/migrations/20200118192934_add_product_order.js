e8280c156fe2a350e09bcc29f876717f6fca7dab
Rasheed Rahman <ra1da35ma@gmail.com>
2020-01-21 07:23:53 +0100

    add hint for each product
    displayed on the add-delivery page during user registration

--- db/migrations/20200118192934_add_product_order.js
+++ db/migrations/20200118192934_add_product_order.js
@@ -3,11 +3,13 @@
 exports.up = function(knex, Promise) {
   return knex.schema.table('product', function(table) {
     table.integer('order').nullable().after('active');
+    table.string('hint').nullable().after('productDesc');
   });
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema.table('product', function(table) {
     table.dropColumn('order');
+    table.dropColumn('hint');
   });
 };
