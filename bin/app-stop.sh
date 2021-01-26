fa6d1f81ad57f33e9dc7a75588cc718e747811ef
Idiakose O. Sunday <idiakosesunday@gmail.com>
2021-01-26 19:50:54 +0100

    Port to pm2

--- bin/app-stop.sh
+++ bin/app-stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-/home/ec2-user/.nvm/versions/node/v8.12.0/bin/forever stopall || true
-/home/ec2-user/.nvm/versions/node/v8.12.0/bin/forever cleanlogs || true
+/home/ec2-user/.nvm/versions/node/v8.12.0/bin/pm2 stop gracetree_server || true
+/home/ec2-user/.nvm/versions/node/v8.12.0/bin/pm2 flush gracetree_server || true
