2403d42d1f6c882bd9bb53a84769267314ea9f80
Idiakose O. Sunday <idiakosesunday@gmail.com>
2021-03-29 20:37:46 +0100

    Upgrade to node v14.16.0
    Use *-slim for docker image

--- bin/app-start.sh
+++ bin/app-start.sh
@@ -4,4 +4,4 @@ export HOST=0.0.0.0
 export PORT=3001
 export SESSION_SECRET='grace.tree.server.io.main.site.ASDEREDDSF@ECxSz/Q'
 export $(cat /home/ec2-user/.env)
-(cd /home/ec2-user/application-server && exec /home/ec2-user/.nvm/versions/node/v8.12.0/bin/pm2 start server.js --name gracetree_server)
+(cd /home/ec2-user/application-server && exec /home/ec2-user/.nvm/versions/node/v14.16.0/bin/pm2 start server.js --name gracetree_server)
